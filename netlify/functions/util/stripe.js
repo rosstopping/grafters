0495f0699269f08070a77ae3adc33a6a58952808
Ross Topping <ross@digizu.co.uk>
2022-12-16 17:34:23 +0000

    wip

---
+++ netlify/functions/util/stripe.js
@@ -0,0 +1,19 @@
+exports.getPaymentMethods = () => [
+  'card',
+  'klarna',
+  // 'alipay',
+  // 'ideal', // EUR only
+  // 'fpx',
+  // 'bacs_debit',
+  // 'bancontact',
+  // 'giropay',
+  // 'p24',
+  // 'eps',
+  // 'sofort', // EUR only
+  // // 'sepa_debit',
+  // 'grabpay',
+];
+
+exports.getShippingCountries = () => [
+  'GB'
+];
