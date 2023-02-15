b0a3140e6ec34967037e47e981e5452b9518500c
Ross Topping <ross@digizu.co.uk>
2023-02-15 08:36:15 +0000

    wip

---
+++ netlify/functions/register-later.js
@@ -0,0 +1,39 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const collect = require('collect.js');
+const axios = require('axios').default;
+// const { getShippingCountries, getPaymentMethods } = require('./util/stripe');
+
+exports.handler = async (event) => {
+    /**
+     * Create the stripe request
+     */
+    let create = {
+        success_url: `${process.env.URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.URL}`,
+        allow_promotion_codes: true,
+        payment_method_types: ['klarna'],
+        billing_address_collection: 'auto',
+        mode: 'payment',
+        line_items: [
+            // {price: 'price_1KFLLpDVeVaT9qPlDiIsYAIy', quantity: 1}, // dev
+            {price: 'price_1LayEvDVeVaT9qPltsbocGOY', quantity: 1}, // live
+        ],
+        customer_email: event.queryStringParameters.email,
+        payment_intent_data: {
+            metadata: event.queryStringParameters
+        }
+    };
+
+    /**
+     * Setup session
+     */
+    const session = await stripe.checkout.sessions.create(create);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            stripe_key: process.env.STRIPE_KEY,
+            sessionId: session.id
+        }),
+    }
+};
